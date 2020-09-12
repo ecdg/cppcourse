aface8f2c8e489e62abc77c67a56d68c4093cdc0
Emily Dayanghirang <emilydynh@gmail.com>
2020-09-11 21:35:06 -0700

    Add files via upload

---
+++ pizzaSlices.cpp
@@ -0,0 +1,31 @@
+/*
+   Calculates the number of slices
+   a pizza of any size can be divided into
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    const double PI = 3.14159, AREA_SLICE = 14.125;
+    double pizzaDiameter, pizzaRadius, areaOfPizza;
+    int numberOfSlices;
+
+    // Prompt user
+    cout << "Enter pizza diameter: ";
+    cin >> pizzaDiameter;
+
+    // Calculations for radius and area of pizza
+    pizzaRadius = pizzaDiameter / 2.0;
+    areaOfPizza = PI * pow(pizzaRadius, 2.0);
+
+    // Calculation for number of slices
+    numberOfSlices = areaOfPizza / AREA_SLICE;
+
+    // Display number of slices
+    cout << numberOfSlices << " slices" << endl;
+
+}
