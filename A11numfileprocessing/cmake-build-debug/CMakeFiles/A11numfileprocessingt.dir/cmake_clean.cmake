854588903c2cf28b7e8f977f56f7afb50a61212d
Emily Dayanghirang <emilydynh@gmail.com>
2020-10-16 20:51:55 -0700

    Add files via upload

---
+++ A11numfileprocessing/cmake-build-debug/CMakeFiles/A11numfileprocessingt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "A11numfileprocessingt"
+  "A11numfileprocessingt.pdb"
+  "CMakeFiles/A11numfileprocessingt.dir/A11numfileprocessing.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A11numfileprocessingt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
